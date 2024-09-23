bc5b4afd11a6d62baf5cc07372692d53bc2df925
r-takahashi <ryuso0720.work@gmail.com>
2024-09-23 12:03:00 +0900

    create_models

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,20 +2,20 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
@@ -24,9 +24,9 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for arrays.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
@@ -34,12 +34,31 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be cast to native types.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+
+    public function reservations()
+    {
+        return $this->belongsToMany(Shop::class, 'reservations')->withPivot('id','date','time','user_num');
+    }
+    public function likes()
+    {
+        return $this->belongsToMany(Shop::class, 'likes');
+    }
+    public static function registration($request)
+    {
+        $hashed_password = Hash::make($request->password);
+        $param = [
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => $hashed_password,
+        ];
+        $user = User::create($param);
+        return $user;
+    }
 }
